bdbaf412ed66a93fd3111b261d34def6418559f6
Nimish Kumar <nimishkumar9534@gmail.com>
2021-10-27 03:39:08 +0530

    main

--- src/Component/Portfolio/Card/Card.js
+++ src/Component/Portfolio/Card/Card.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import cardStyle from './Card.module.css'
 import {AiOutlinePlus} from 'react-icons/ai'
+import img from '../../images/portfolio/brochure.PNG'
 
 const card = () => {
     return (
         <>
             <div className={cardStyle.container}>
+            <img src={img} alt="portfolio" className={cardStyle.image}/>
                 <div className={cardStyle.description}>
                     <h3 className={cardStyle.heading}>javacript quiz game</h3>
                     <div className={cardStyle.card_parent}>
