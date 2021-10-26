b20d6f34178296d4882d2d70db2df16d5645b5da
Nimish Kumar <nimishkumar9534@gmail.com>
2021-10-26 20:02:45 +0530

    icon color change

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import homeStyle from "./Home.module.css";
-import {FaFacebookF, FaInstagram, FaLinkedin, FaGithub} from 'react-icons/fa'
+import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
 import Menu from "../Menu/Menu";
 
 const Home = () => {
@@ -8,7 +8,7 @@ const Home = () => {
     <>
       <div className={homeStyle.container}>
         <div className={homeStyle.overlay}>
-        <Menu />
+          <Menu />
           <div className={homeStyle.content}>
             <h5 className={homeStyle.heading}>hello, World</h5>
             <h1 className={homeStyle.sub_heading}>I'm Nimish Kumar</h1>
@@ -16,10 +16,9 @@ const Home = () => {
             <button className={homeStyle.btn}>More about me</button>
           </div>
           <div className={homeStyle.links}>
-            <FaFacebookF className={homeStyle.icon}/>
-            <FaInstagram className={homeStyle.icon}/>
-            <FaLinkedin className={homeStyle.icon}/>
-            <FaGithub className={homeStyle.icon}/>
+            <a href="https://www.instagram.com/nimish_kumar_kashyap/"><FaInstagram className={homeStyle.icon} /></a>
+            <FaLinkedin className={homeStyle.icon} />
+            <FaGithub className={homeStyle.icon} />
           </div>
         </div>
       </div>
