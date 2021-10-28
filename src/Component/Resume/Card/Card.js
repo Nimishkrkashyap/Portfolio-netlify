0de57928a969c55e9ff7a8149a7e9eabcc6de719
Nimish Kumar <nimishkumar9534@gmail.com>
2021-10-28 14:52:33 +0530

    card

--- src/Component/Resume/Card/Card.js
+++ src/Component/Resume/Card/Card.js
@@ -6,19 +6,24 @@ const Card = () => {
   return (
     <>
       <div className={cardStyle.container}>
+
         <div className={cardStyle.left}>
           <h1 className={cardStyle.heading}>UI Designer</h1>
           <h3 className={cardStyle.sub_heading}>July 2015 - Present</h3>
         </div>
+
+        <div className={cardStyle.dark_line_mobile}></div>
+
         <div className={cardStyle.center}>
           <div className={cardStyle.icon_parent}>
             <FaGraduationCap className={cardStyle.icon} />
           </div>
           <div className={cardStyle.light_line}></div>
         </div>
+        
         <div className={cardStyle.right}>
           <h1 className={cardStyle.heading}>Awesome Studio</h1>
-          <div className={cardStyle.dark_line}></div>
+          <div className={cardStyle.dark_line_desktop}></div>
           <p className={cardStyle.para}>
             Lorem ipsum Occaecat do esse ex et dolor culpa nisi ex in magna
             consectetur nisi cupidatat laboris esse eiusmod deserunt aute do
